6c79cdadf419c399089faf504d52b47d2f22b9f4
NaomiBid <naomi0bid@gmail.com>
2018-12-03 12:00:22 +0000

    deposit method adds deposit amount to balance

--- bank_tech_test/spec/account_spec.rb
+++ bank_tech_test/spec/account_spec.rb
@@ -7,10 +7,11 @@ describe Account do
   end
 
   describe '#deposit' do
-    it "adds the deposit amount to the current balance"
-    account = Account.new
-    account.deposit(1000)
-    expect(subject.balance).to eq 1000
+    it "adds the deposit amount to the current balance" do
+      account = Account.new
+      account.deposit(1000)
+      expect(account.balance).to eq 1000
+    end
   end
 
 end
