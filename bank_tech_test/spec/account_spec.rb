9de744108c0844342c4ff1fd27a530b883c3988f
NaomiBid <naomi0bid@gmail.com>
2018-12-03 11:41:08 +0000

    test for deposit method

--- bank_tech_test/spec/account_spec.rb
+++ bank_tech_test/spec/account_spec.rb
@@ -1,7 +1,16 @@
 require 'account.rb'
 
 describe Account do
+
   it "new account has a balance of 0" do
     expect(subject.balance).to eq 0
   end
+
+  describe '#deposit' do
+    it "adds the deposit amount to the current balance"
+    account = Account.new
+    account.deposit(1000)
+    expect(subject.balance).to eq 1000
+  end
+
 end
