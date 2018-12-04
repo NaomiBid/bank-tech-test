80fcbab3463f30cc2347ab13b11756fce469a219
NaomiBid <naomi0bid@gmail.com>
2018-12-04 15:19:40 +0000

    test for printing statement

--- bank_tech_test/spec/statement_spec.rb
+++ bank_tech_test/spec/statement_spec.rb
@@ -14,17 +14,18 @@ describe Statement do
       expect(account.statement.transaction_list).to eq [@fake_time, true, 1000, 1000]
     end
   end
-# describe "#" do
-#    before do
-#      @fake_time = Time.now
-#      Time.stub(:now) { @fake_time }
-#    end
-#    it "prints out the transaction list in a statement format" do
-#      account = Account.new
-#      account.transaction(true, 1000)
-#      p @transaction_list
-#      expect(account.statement).to eq "#{@fake_time}  ||  true  ||  1000  ||  1000"
-#    end
-#  end
+
+ describe "#print_statement" do
+    before do
+      @fake_time = Time.now
+      Time.stub(:now) { @fake_time }
+    end
+    
+    it "prints out the transaction list in a statement format" do
+      account = Account.new
+      account.transaction(true, 1000)
+      expect(account.statement.print_statement).to eq "#{@fake_time}  ||  true  ||  1000  ||  1000"
+    end
+  end
 
 end
